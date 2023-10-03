d08bc7962999181ddacd86dde7859b223252139a
Hasin Hayder <countdraculla@gmail.com>
2023-10-03 23:29:05 +0600

    Episode 01

--- components/src/App.vue
+++ components/src/App.vue
@@ -14,6 +14,7 @@
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Button
       </button>
+      <Button color="orange" label="Click me"/>
     </div>
     
   </div>
